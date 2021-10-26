a88d2040ad3521ed7a32628ad01234c08ad1f505
Daniel Eduardo Medina <danielmedinacs97@gmail.com>
2021-10-26 12:13:56 -0700

    Initial Commit. Added title, search bar, and author names.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+  ];
+
+const App = () => {
+  const handleChange = event => {
+    console.log(event);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>My Hacker Stories</h1>
+
+     <label htmlFor="search">Search: </label>
+     <input id="search" type="text"/>
+
+     <hr />
+    
+    <List />
+    
     </div>
   );
 }
+
+ const List = () => {
+  return list.map(function(item) {
+  return (
+  <div key={item.objectID}>
+  <span>
+  <a href={item.url}>{item.titlse}</a>
+  </span>
+  <span> {item.author}</span>
+  <span> {item.num_comments}</span>
+  <span>{item.points}</span>
+  </div>
+  );
+  });
+  }
 
 export default App;
