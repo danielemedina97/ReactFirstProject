56a1e359875cd690d5161b41a58fb4284bb7e46c
Daniel Eduardo Medina <danielmedinacs97@gmail.com>
2021-10-27 12:30:47 -0700

    Added Search component to react app

--- src/App.js
+++ src/App.js
@@ -2,47 +2,61 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-  },
-  {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-  },
-  ];
+
 
 const App = () => {
-  const handleChange = event => {
-    console.log(event);
-  };
+  const stories = [
+    {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+    },
+    {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+    },
+    ];
 
   return (
     <div>
      <h1>My Hacker Stories</h1>
-
-     <label htmlFor="search">Search: </label>
-     <input id="search" type="text"/>
+    
+     <Search />
 
      <hr />
     
-    <List />
+    <List list={stories}/>
     
     </div>
   );
 }
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+  
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+    </div>
+   );
+  };
 
- const List = () => {
-  return list.map(function(item) {
+ const List = props => {
+  return props.list.map(function(item) {
   return (
   <div key={item.objectID}>
   <span>
